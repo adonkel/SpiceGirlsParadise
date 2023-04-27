4f0f05e54eccdea8c69de345a68bd4c3cb3e08ec
Will Alexander <alexanderwill499@gmail.com>
2023-04-27 16:08:03 -0400

    CICD 6

--- s3.tf
+++ s3.tf
@@ -34,37 +34,37 @@ data "aws_iam_policy_document" "backend" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "backend" {
-#   bucket = aws_s3_bucket.backend.id
-#   policy = data.aws_iam_policy_document.backend.json
-# }
+resource "aws_s3_bucket_policy" "backend" {
+  bucket = aws_s3_bucket.backend.id
+  policy = data.aws_iam_policy_document.backend.json
+}
 
-# resource "aws_kms_key" "backend" {
-#   description             = "This key is used to encrypt bucket objects"
-#   deletion_window_in_days = 16
-# }
+resource "aws_kms_key" "backend" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 16
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
-#    bucket = aws_s3_bucket.backend.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+   bucket = aws_s3_bucket.backend.id
 
-  # rule {
-  #   apply_server_side_encryption_by_default {
-  #     kms_master_key_id = aws_kms_key.backend.arn
-  #     sse_algorithm     = "aws:kms"
-  #   }
-  # }
-#}
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.backend.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
 
 
 
-# resource "aws_s3_bucket_acl" "log_bucket_acl" {
-#   bucket = aws_s3_bucket.backend.id
-#   acl    = "log-delivery-write"
-# }
+resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  bucket = aws_s3_bucket.backend.id
+  acl    = "log-delivery-write"
+}
 
-# resource "aws_s3_bucket_logging" "backend" {
-#    bucket = aws_s3_bucket.backend.id
+resource "aws_s3_bucket_logging" "backend" {
+   bucket = aws_s3_bucket.backend.id
 
-#   target_bucket = aws_s3_bucket.backend.id
-#   target_prefix = "log/"
-#}
+  target_bucket = aws_s3_bucket.backend.id
+  target_prefix = "log/"
+}
